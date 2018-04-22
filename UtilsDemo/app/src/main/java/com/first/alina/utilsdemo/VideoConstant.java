f7cb905b68d0d44657518c76642323e1d93170a3
huangzhiqin <691483623@qq.com>
2018-04-22 16:20:02 +0800

    添加视频

---
+++ UtilsDemo/app/src/main/java/com/first/alina/utilsdemo/VideoConstant.java
@@ -0,0 +1,114 @@
+package com.first.alina.utilsdemo;
+
+/**
+ * Created by shengjun on 16/9/10.
+ */
+public class VideoConstant {
+
+    public static String[] videoUrlList =
+            {
+                    "http://image.cailianpress.com/admin/20180323/111944tT9Usc76pUCf.mp4",
+                    "http://image.cailianpress.com/admin/20180323/140532zijIYKBiwQGg.mp4",
+                    "http://image.cailianpress.com/admin/20180323/140532zijIYKBiwQGg.mp4",
+                    "http://image.cailianpress.com/admin/20180323/140532zijIYKBiwQGg.mp4",
+                    "http://image.cailianpress.com/admin/20180323/140532zijIYKBiwQGg.mp4",
+                    "http://image.cailianpress.com/admin/20180323/111944tT9Usc76pUCf.mp4",
+                    "http://image.cailianpress.com/admin/20180323/140532zijIYKBiwQGg.mp4"
+            };
+
+    public static String[] videoThumbList =
+            {
+                    "http://img.zcool.cn/community/01e0bc57d512250000012e7e16c5c0.jpg@1280w_1l_2o_100sh.jpg",
+                    "http://img.zcool.cn/community/01e0bc57d512250000012e7e16c5c0.jpg@1280w_1l_2o_100sh.jpg",
+                    "http://img.zcool.cn/community/01e0bc57d512250000012e7e16c5c0.jpg@1280w_1l_2o_100sh.jpg",
+                    "http://img.zcool.cn/community/01e0bc57d512250000012e7e16c5c0.jpg@1280w_1l_2o_100sh.jpg",
+                    "http://img.zcool.cn/community/01e0bc57d512250000012e7e16c5c0.jpg@1280w_1l_2o_100sh.jpg",
+                    "http://img.zcool.cn/community/01e0bc57d512250000012e7e16c5c0.jpg@1280w_1l_2o_100sh.jpg",
+                    ""
+            };
+
+    public static String[][] videoUrls =
+            {
+                    {
+                            "http://image.cailianpress.com/admin/20180323/111944tT9Usc76pUCf.mp4",
+                            "http://image.cailianpress.com/admin/20180323/140532zijIYKBiwQGg.mp4",
+                            "http://image.cailianpress.com/admin/20180323/111944tT9Usc76pUCf.mp4",
+                            "http://image.cailianpress.com/admin/20180323/111944tT9Usc76pUCf.mp4",
+                            "http://image.cailianpress.com/admin/20180323/140532zijIYKBiwQGg.mp4",
+                            "http://image.cailianpress.com/admin/20180323/140532zijIYKBiwQGg.mp4",
+                            "http://image.cailianpress.com/admin/20180323/111944tT9Usc76pUCf.mp4",
+                            "http://image.cailianpress.com/admin/20180323/140532zijIYKBiwQGg.mp4",
+                            "http://image.cailianpress.com/admin/20180323/111944tT9Usc76pUCf.mp4",
+                            "http://image.cailianpress.com/admin/20180323/111944tT9Usc76pUCf.mp4"
+                    },
+                    {
+                            "http://image.cailianpress.com/admin/20180323/140532zijIYKBiwQGg.mp4",
+                            "http://image.cailianpress.com/admin/20180323/140532zijIYKBiwQGg.mp4",
+                            "http://image.cailianpress.com/admin/20180323/111944tT9Usc76pUCf.mp4",
+                            "http://image.cailianpress.com/admin/20180323/140532zijIYKBiwQGg.mp4"
+                    },
+                    {
+                            "http://image.cailianpress.com/admin/20180323/140532zijIYKBiwQGg.mp4",
+                            "http://image.cailianpress.com/admin/20180323/140532zijIYKBiwQGg.mp4",
+                            "http://image.cailianpress.com/admin/20180323/111944tT9Usc76pUCf.mp4",
+                            "http://image.cailianpress.com/admin/20180323/140532zijIYKBiwQGg.mp4"
+                    }
+            };
+
+    public static String[][] videoThumbs =
+            {
+                    {
+                            "http://img.zcool.cn/community/01e0bc57d512250000012e7e16c5c0.jpg@1280w_1l_2o_100sh.jpg",
+                            "http://img.zcool.cn/community/01e0bc57d512250000012e7e16c5c0.jpg@1280w_1l_2o_100sh.jpg",
+                            "http://img.zcool.cn/community/01e0bc57d512250000012e7e16c5c0.jpg@1280w_1l_2o_100sh.jpg",
+                            "http://img.zcool.cn/community/01e0bc57d512250000012e7e16c5c0.jpg@1280w_1l_2o_100sh.jpg",
+                            "http://img.zcool.cn/community/01e0bc57d512250000012e7e16c5c0.jpg@1280w_1l_2o_100sh.jpg",
+                            "http://img.zcool.cn/community/01e0bc57d512250000012e7e16c5c0.jpg@1280w_1l_2o_100sh.jpg",
+                            "http://img.zcool.cn/community/01e0bc57d512250000012e7e16c5c0.jpg@1280w_1l_2o_100sh.jpg",
+                            "http://img.zcool.cn/community/01e0bc57d512250000012e7e16c5c0.jpg@1280w_1l_2o_100sh.jpg",
+                            "http://img.zcool.cn/community/01e0bc57d512250000012e7e16c5c0.jpg@1280w_1l_2o_100sh.jpg",
+                            "http://img.zcool.cn/community/01e0bc57d512250000012e7e16c5c0.jpg@1280w_1l_2o_100sh.jpg"
+                    },
+                    {
+                            "http://img.zcool.cn/community/01e0bc57d512250000012e7e16c5c0.jpg@1280w_1l_2o_100sh.jpg",
+                            "http://img.zcool.cn/community/01e0bc57d512250000012e7e16c5c0.jpg@1280w_1l_2o_100sh.jpg",
+                            "http://img.zcool.cn/community/01e0bc57d512250000012e7e16c5c0.jpg@1280w_1l_2o_100sh.jpg",
+                            "http://img.zcool.cn/community/01e0bc57d512250000012e7e16c5c0.jpg@1280w_1l_2o_100sh.jpg"
+                    },
+                    {
+                            "http://img.zcool.cn/community/01e0bc57d512250000012e7e16c5c0.jpg@1280w_1l_2o_100sh.jpg",
+                            "http://img.zcool.cn/community/01e0bc57d512250000012e7e16c5c0.jpg@1280w_1l_2o_100sh.jpg",
+                            "http://img.zcool.cn/community/01e0bc57d512250000012e7e16c5c0.jpg@1280w_1l_2o_100sh.jpg",
+                            "http://img.zcool.cn/community/01e0bc57d512250000012e7e16c5c0.jpg@1280w_1l_2o_100sh.jpg"
+                    }
+            };
+
+    public static String[][] videoTitles =
+            {
+                    {
+                            "视频",
+                            "视频",
+                            "视频",
+                            "视频",
+                            "视频",
+                            "视频",
+                            "视频",
+                            "视频",
+                            "视频",
+                            "视频"
+                    },
+                    {
+                            "视频",
+                            "视频",
+                            "视频",
+                            "视频"
+                    },
+                    {
+                            "视频",
+                            "视频",
+                            "视频",
+                            "视频"
+                    }
+            };
+
+}
