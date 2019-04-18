d9619924cbd9f4065a0af3fa153210513a9c78a4
huangzhiqin <691483623@qq.com>
2019-04-18 16:57:19 +0800

    加入线程池ScheduledExecutorService

--- UtilsDemo/app/src/main/java/com/first/alina/utilsdemo/thread_demo/ThreadDemoActivity.java
+++ UtilsDemo/app/src/main/java/com/first/alina/utilsdemo/thread_demo/ThreadDemoActivity.java
@@ -6,30 +6,34 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.util.Log;
 import android.view.View;
-import android.widget.Toast;
 
 import com.first.alina.utilsdemo.R;
+import com.first.alina.utilsdemo.thread_demo.executorservice.SingleThreadExecutor;
 
 import java.util.concurrent.Callable;
+import java.util.concurrent.Executors;
 import java.util.concurrent.FutureTask;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by alina on 2019/4/16.
  */
 
-public class ThreadDemoActivity extends Activity{
-    private final String TAG="ThreadDemoActivity";
+public class ThreadDemoActivity extends Activity {
+    private final String TAG = "ThreadDemoActivity";
     private MyAsyncTask myAsyncTask;
+    private ScheduledExecutorService scheduledExecutorService;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_thread_demo);
         new MyThread().start();
-        MyThreadRunnable myThreadRunnable=new MyThreadRunnable();
+        MyThreadRunnable myThreadRunnable = new MyThreadRunnable();
         new Thread(myThreadRunnable).start();
-        MyCallableThread myCallableThread=new MyCallableThread();
-        FutureTask<Integer> futureTask=new FutureTask<Integer>(myCallableThread);
+        MyCallableThread myCallableThread = new MyCallableThread();
+        FutureTask<Integer> futureTask = new FutureTask<Integer>(myCallableThread);
         futureTask.cancel(true);
         new Thread(futureTask).start();
 
@@ -39,42 +43,44 @@ public class ThreadDemoActivity extends Activity{
 
         myAsyncTask = new MyAsyncTask("AsyncTask1");
         //myAsyncTask.execute("01");
-       // new MyAsyncTask("AsyncTask2").execute("02");
+        // new MyAsyncTask("AsyncTask2").execute("02");
 
         findViewById(R.id.start_aysnctask).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                myAsyncTask.execute("01");
+                //myAsyncTask.execute("01");
+                newSingleThreadScheduledExecutor();
             }
         });
 
         findViewById(R.id.close_aysnctask).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                myAsyncTask.cancel(true);
+                //myAsyncTask.cancel(true);
+                scheduledExecutorService.shutdown();
             }
         });
 
 
     }
 
-    private class MyThread extends Thread{
+    private class MyThread extends Thread {
         @Override
         public void run() {
             super.run();
-            Log.e(TAG,"===>name="+currentThread().getName());
+            Log.e(TAG, "===>name=" + currentThread().getName());
         }
 
     }
 
-    private class MyThreadRunnable implements Runnable{
+    private class MyThreadRunnable implements Runnable {
         @Override
         public void run() {
-            Log.e(TAG,"===>name="+Thread.currentThread().getName());
+            Log.e(TAG, "===>name=" + Thread.currentThread().getName());
         }
     }
 
-    private class MyCallableThread implements Callable<Integer>{
+    private class MyCallableThread implements Callable<Integer> {
 
         @Override
         public Integer call() throws Exception {
@@ -82,7 +88,7 @@ public class ThreadDemoActivity extends Activity{
         }
     }
 
-    private class MyAsyncTask extends AsyncTask<String ,Integer ,String>{
+    private class MyAsyncTask extends AsyncTask<String, Integer, String> {
         private String name;
         private volatile boolean isCanceled;
 
@@ -97,35 +103,50 @@ public class ThreadDemoActivity extends Activity{
 
         @Override
         protected String doInBackground(String... params) {
-            for (int i=0;i<100000;i++){
-                if (isCancelled()){
-                    Log.e(TAG,"============> doInBackground 取消");
+            for (int i = 0; i < 100000; i++) {
+                if (isCancelled()) {
+                    Log.e(TAG, "============> doInBackground 取消");
                     break;
                 }
             }
             return params[0];
         }
+
         @Override
         protected void onProgressUpdate(Integer... values) {
             super.onProgressUpdate(values);
-            Log.e(TAG,"============> onProgressUpdate "+values[0]);
+            Log.e(TAG, "============> onProgressUpdate " + values[0]);
 
         }
+
         @Override
         protected void onCancelled(String s) {
             super.onCancelled(s);
-            Log.e(TAG,"============> 取消 "+s);
+            Log.e(TAG, "============> 取消 " + s);
         }
+
         @Override
         protected void onCancelled() {
             super.onCancelled();
-            Log.e(TAG,"============> 取消 ");
+            Log.e(TAG, "============> 取消 ");
         }
     }
+
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        Log.e(TAG,"===========>取消 onDestroy");
+        Log.e(TAG, "===========>取消 onDestroy");
         myAsyncTask.cancel(true);
+    }
+
+    private void newSingleThreadScheduledExecutor(){
+          scheduledExecutorService = Executors.newSingleThreadScheduledExecutor();
+          scheduledExecutorService.scheduleAtFixedRate(new Runnable() {
+            @Override
+            public void run() {
+                Log.e(TAG,"===========> "+Thread.currentThread().getName()+"   ");
+            }
+        },0,2,TimeUnit.SECONDS);
+
     }
 }
---
+++ UtilsDemo/app/src/main/java/com/first/alina/utilsdemo/thread_demo/executorservice/SingleThreadExecutor.java
@@ -0,0 +1,24 @@
+package com.first.alina.utilsdemo.thread_demo.executorservice;
+
+import android.util.Log;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by alina on 2019/4/18.
+ * 只有一个线程的线程池newSingleThreadExecutor
+ * 为什么只有一个线程还叫做线程池？
+ */
+
+public class SingleThreadExecutor {
+
+    private static final String TAG="SingleThreadExecutor";
+    public static void init(){
+
+
+    }
+
+
+}
